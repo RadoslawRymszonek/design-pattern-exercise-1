a7b9bf085b3d09636a4325e4baf0e5696d8c1047
Radoslaw Rymszonek <punisherldz@gmail.com>
2019-10-19 11:09:19 +0200

    Done

--- src/main/java/chart/BarChart.java
+++ src/main/java/chart/BarChart.java
@@ -125,4 +125,67 @@ public class BarChart {
     public void setDisplayLegend(boolean displayLegend) {
         this.displayLegend = displayLegend;
     }
+
+    public static BarChartBuilder builder(){
+        return new BarChartBuilder();
 }
+
+public static class BarChartBuilder {
+    private String title;
+    private String fontName;
+    private int fontSize;
+    private String fontColor;
+    private int width;
+    private int height;
+    private String backgroundColor;
+    private List<String> data;
+    private boolean displayTitle;
+    private boolean displayLegend;
+
+    public BarChartBuilder(){
+    }
+
+    public BarChartBuilder withTitle(String title){
+        this.title = title;
+        return this;
+    }
+    public BarChartBuilder withFontName(String fontName){
+        this.fontName = fontName;
+        return this;
+    }
+    public BarChartBuilder withFontSize(int fontSize){
+        this.fontSize = fontSize;
+        return this;
+    }
+    public BarChartBuilder withFontColor(String fontColor){
+        this.fontColor = fontColor;
+        return this;
+    }
+    public BarChartBuilder withWidth(int width){
+        this.width = width;
+        return this;
+    }
+    public BarChartBuilder withHeight(int height){
+        this.height = height;
+        return this;
+    }
+    public BarChartBuilder withBackgroundColor(String backgroundColor){
+        this.backgroundColor = backgroundColor;
+        return this;
+    }
+    public BarChartBuilder withData(List data){
+        this.data = data;
+        return this;
+    }
+    public BarChartBuilder withDisplayTitle(boolean displayTitle){
+        this.displayLegend = displayTitle;
+        return this;
+    }
+    public BarChartBuilder withDisplayLegend(boolean displayLegend){
+        this.displayLegend = displayLegend;
+        return this;
+    }
+
+}
+}
+
